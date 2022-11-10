72b9981b8e05c992f4be42c9e3cce53c365c1adb
YMCX <89810988+ymcx@users.noreply.github.com>
2022-11-10 20:30:47 +0200

    Update Pluginblock.js

--- app/src/main/java/xyz/webtubeapp/Pluginblock.js
+++ app/src/main/java/xyz/webtubeapp/Pluginblock.js
@@ -1,25 +1,4 @@
-/**
- * This file is part of AdGuard's Block YouTube Ads (https://github.com/AdguardTeam/BlockYouTubeAdsShortcut).
- *
- * AdGuard's Block YouTube Ads is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * AdGuard's Block YouTube Ads is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with AdGuard's Block YouTube Ads.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- * The function that implements all the logic.
- * Returns the run status.
- */
-export function runBlockYoutube() {
+function runBlockYoutube() {
     const locales = {
         en: {
             logo: 'with&nbsp;AdGuard',
