cf8464222e6816e3ce74842ab38aed1d353efcc2
YMCX <89810988+ymcx@users.noreply.github.com>
2022-11-10 23:16:34 +0200

    Update JSController.kt

--- app/src/main/java/xyz/webtubeapp/JSController.kt
+++ app/src/main/java/xyz/webtubeapp/JSController.kt
@@ -5,38 +5,39 @@ import android.webkit.WebView
 
 class JSController(webView: WebView, context : Context) {
     private val webView: WebView = webView
-    var js = "[{url: 'https://raw.githubusercontent.com/thewebtube/plugins/main/adguard/main.js'}];"
-    fun exec(action: String) {
+    var js = "[{url: 'https://raw.githubusercontent.com/AdguardTeam/BlockYouTubeAdsShortcut/master/dist/index.js'}]"
+    fun exec() {
         webView.evaluateJavascript("""
-        (() => {
-        var plugins = $js
-          var cache = {};
-            function injectAll(mode = "all") {
-              for (var i = 0; i < plugins.length; i++) {
-                  if ((plugins[i].injectOnUrlChange || mode == "all")) {
-                      injectScript(plugins[i].url);
-                  }
-              }
-            }
-            function injectScript(url) {
-              if (cache[url]) {
-                  eval(cache[url]);
-              } else {
-                  var xhr = new XMLHttpRequest();
-                  xhr.open("GET", url, true);
-                  xhr.onreadystatechange = function () {
-                      if (xhr.readyState == 4) {
-                          if (xhr.status == 200) {
-                          cache[url] = xhr.responseText;
-                          eval(xhr.responseText);
-                          }
-                      }
-                  }
-                  xhr.send();
-              }
-            }
-            injectAll("all")
-          })();
-    """.trimIndent(), null)
+            (() => {
+                const style = document.createElement('style');
+                style.textContent = `body {-webkit-tap-highlight-color:transparent !Important;}`;
+                document.head.append(style);
+                var plugins = $js
+                var cache = {};
+                function injectAll() {
+                    for (var i = 0; i < plugins.length; i++) {
+                        injectScript(plugins[i].url);
+                    }
+                }
+                function injectScript(url) {
+                    if (cache[url]) {
+                        eval(cache[url]);
+                    } else {
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("GET", url, true);
+                        xhr.onreadystatechange = function () {
+                            if (xhr.readyState == 4) {
+                                if (xhr.status == 200) {
+                                    cache[url] = xhr.responseText;
+                                    eval(xhr.responseText);
+                                }
+                            }
+                        }
+                        xhr.send();
+                    }
+                }
+                injectAll()
+            })();
+        """.trimIndent(), null)
     }
 }
