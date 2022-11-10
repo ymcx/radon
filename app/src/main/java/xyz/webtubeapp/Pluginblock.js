bad3cd4eb9291e5d4a104389b4493f853a82485d
YMCX <89810988+ymcx@users.noreply.github.com>
2022-11-10 20:28:25 +0200

    Update Pluginblock.js

--- app/src/main/java/xyz/webtubeapp/Pluginblock.js
+++ app/src/main/java/xyz/webtubeapp/Pluginblock.js
@@ -455,3 +455,19 @@ export function runBlockYoutube() {
         message: getMessage('success'),
     };
 }
+
+(() => {
+    // "completion" function is only defined if this script is launched as Shortcut
+    // in other cases we simply polyfill it.
+    let finish = (m) => { console.log(m); };
+    if (typeof completion !== 'undefined') {
+        finish = completion;
+    }
+
+    try {
+        const result = runBlockYoutube();
+        finish(result.message);
+    } catch (ex) {
+        finish(ex.toString());
+    }
+})();
