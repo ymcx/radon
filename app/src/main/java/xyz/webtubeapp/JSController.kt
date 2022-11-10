964777b5407c1958c9ff94adeb2150d7eb074622
YMCX <89810988+ymcx@users.noreply.github.com>
2022-11-10 21:36:01 +0200

    Update JSController.kt

--- app/src/main/java/xyz/webtubeapp/JSController.kt
+++ app/src/main/java/xyz/webtubeapp/JSController.kt
@@ -1,58 +1,69 @@
 package xyz.webtubeapp
 
 import android.content.Context
+import android.provider.Settings.Global.getString
 import android.webkit.WebView
 
 class JSController(webView: WebView, context : Context) {
     private val webView: WebView = webView
-    var pluginsRepo : PluginsRepo = PluginsRepo(context)
     init {
         this.exec("init")
     }
-    private val initScript = """
-        const style = document.createElement('style');
-        style.textContent = `body {-webkit-tap-highlight-color:transparent !Important;}`;
-        document.head.append(style);
-        if (!window.executed) {
-            (() => {
-                ${pluginsRepo.getJsScript()}
-            })();
-            window.addEventListener(
-                "visibilitychange",
-                function (event) {
-                    event.stopImmediatePropagation();
-                },
-                true
-            );
-            window.addEventListener(
-                "webkitvisibilitychange",
-                function (event) {
-                    event.stopImmediatePropagation();
-                },
-                true
-            );
-            window.addEventListener(
-                "blur",
-                function (event) {
-                    event.stopImmediatePropagation();
-                },
-                true
-            );
-            setInterval(()=> {
-                try { 
-                    document.querySelector("#app > div.page-container > ytm-browse > ytm-single-column-browse-results-renderer > div > div > ytm-sign-in-promo-with-background-renderer > ytm-promo > a.sign-in-link").href="https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=false&continue=https://m.youtube.com/"
-                } catch(e) {}
-                try {
-                    document.querySelector("#menu > div > ytm-multi-page-menu-renderer > div > ytm-multi-page-menu-section-renderer:nth-child(2) > ytm-compact-link-renderer > a").href="https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=false&continue=https://m.youtube.com/"
-                } catch(e) {}
-                try {
-                    document.querySelector("#menu > div > ytm-multi-page-menu-renderer > div > ytm-multi-page-menu-section-renderer > ytm-compact-link-renderer:nth-child(1) > a").href="https://accounts.google.com/signin/v2/identifier?continue=https://m.youtube.com/&app=m&next=%2F&passive=false&service=youtube&uilel=0&flowName=GlifWebSignIn&flowEntry=AddSession"
-                } catch(e) {}
-            }
-            ,1000)
-            window.executed = true
-        }
-    """.trimIndent()
+
+var pluginsjs = "[{name: 'AdGuard', url: 'https://raw.githubusercontent.com/thewebtube/plugins/main/adguard/main.js', injectOnUrlChange: false, enabled: true},\n];"
+
+        var script :String = """
+            
+            var plugins = $pluginsjs
+            
+              var cache = {};
+                
+                function injectAll(mode = "all") {
+                  for (var i = 0; i < plugins.length; i++) {
+                      if (plugins[i].enabled && (plugins[i].injectOnUrlChange || mode == "all")) {
+                          injectScript(plugins[i].url);
+                      }
+                  }
+                }
+                
+                
+                function injectScript(url) {
+                  if (cache[url]) {
+                      console.log("Injecting " + url + " from cache");
+                      eval(cache[url]);
+                  } else {
+                      var xhr = new XMLHttpRequest();
+                      xhr.open("GET", url, true);
+                      xhr.onreadystatechange = function () {
+                          if (xhr.readyState == 4) {
+                              if (xhr.status == 200) {
+                              cache[url] = xhr.responseText;
+                              eval(xhr.responseText);
+                              }
+                          }
+                      }
+                      xhr.send();
+                  }
+                }
+                
+                // on page change 
+                
+                oldurl = window.location.href;
+                
+                setInterval(function () {
+                  if (oldurl != window.location.href) {
+                      oldurl = window.location.href;
+                      injectAll("page change");
+                  }
+                }
+                  , 1000);
+                
+                injectAll("all")
+              
+        """.trimIndent();
+
+    private val initScript = """(() => {${script}})();""".trimIndent()
+
     fun exec(action: String) {
         var script = ""
         if (action == "init") {
@@ -65,4 +76,7 @@ class JSController(webView: WebView, context : Context) {
             }
         }
     }
-}
+    }
+
+
+
