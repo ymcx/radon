dc3626abe06ca5dafcfd5b86dd1b9b330d997087
ymcx <89810988+ymcx@users.noreply.github.com>
2022-11-22 10:19:42 +0200

    Update AdBlock.js

--- app/src/main/java/com/ymcx/radon/AdBlock.js
+++ app/src/main/java/com/ymcx/radon/AdBlock.js
@@ -2,7 +2,7 @@
     const pageScript = () => {
       const hideElements = (hostname) => {
         const style = document.createElement("style");
-        style.innerHTML = `"ytm-channel-list-sub-menu-renderer" {display:none!important;} \n "ytm-companion-ad-renderer" {display:none!important;} \n "ytm-companion-slot" {display:none!important;} \n "ytm-promoted-sparkles-web-renderer" {display:none!important;} \n "ytm-promoted-video-renderer" {display:none!important;} \n body {-webkit-tap-highlight-color:transparent !important;}`;
+        style.innerHTML = `"ytm-channel-list-sub-menu-renderer" {display:none !important;} \n "ytm-companion-ad-renderer" {display:none !important;} \n "ytm-companion-slot" {display:none !important;} \n "ytm-promoted-sparkles-web-renderer" {display:none !important;} \n "ytm-promoted-video-renderer" {display:none !important;} \n body {-webkit-tap-highlight-color:transparent !important;}`;
         document.head.appendChild(style);
       };
       const hideDynamicAds = () => {
