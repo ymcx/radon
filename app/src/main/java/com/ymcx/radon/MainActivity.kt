ab6ac4771001910af4a5b8901888a1fe31020ca9
ymcx <89810988+ymcx@users.noreply.github.com>
2022-11-22 10:08:45 +0200

    Update MainActivity.kt

--- app/src/main/java/com/ymcx/radon/MainActivity.kt
+++ app/src/main/java/com/ymcx/radon/MainActivity.kt
@@ -18,13 +18,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
-class CustomWebView : WebView {
-    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
-    override fun onWindowVisibilityChanged(visibility: Int) {
-        if (visibility != GONE && visibility != INVISIBLE || !this.url.toString().contains("youtube.com/watch?v=")) super.onWindowVisibilityChanged(visibility)
-    }
-}
-
 class MainActivity : AppCompatActivity() {
     private var urlFinished: String = ""
     var webView: WebView? = null
