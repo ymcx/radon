d0708fa68212accd5cb0dd031eb53f55a5a1941f
ymcx <89810988+ymcx@users.noreply.github.com>
2022-12-12 16:20:37 +0200

    Update MainActivity.kt

--- app/src/main/java/com/ymcx/radon/MainActivity.kt
+++ app/src/main/java/com/ymcx/radon/MainActivity.kt
@@ -61,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         }
         webView!!.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                if (request.url.host == "m.youtube.com" || request.url.host == "youtube.com" || request.url.host == "www.youtube.com" || request.url.host == "youtu.be" || request.url.host!!.contains("accounts")) {
+                if (request.url.host!!.contains("youtu")) {
                     return false
                 }
                 Intent(Intent.ACTION_VIEW, request.url).apply {
